a6d58d72b61b1b4251856f54b0b9ab750f3378ca
brookericson <eri11006@byui.edu>
2018-03-21 12:27:47 -0600

    fixed bug

--- src/components/Recommendations/Recommendations.js
+++ src/components/Recommendations/Recommendations.js
@@ -12,12 +12,16 @@ class Recommendations extends Component {
     componentDidMount(){
         const itemId = this.props.itemId;
         const url = "http://api.walmartlabs.com/v1/nbp?apiKey=gtcgtdg945mwxj4ywafatz2w&itemId=" + itemId;
-        Request.get(url).then((response) => {
+        Request.get(url,{
+            headers: {
+                "Accept": "application/json"
+            }
+        }).then((response) => {
             this.setState({
                 showRecommendation: true,
                 recommendations: response.body
             });
-        })
+        });
     }
 
 
